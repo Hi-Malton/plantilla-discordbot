f648ee967ad4a57825938c9c69caa6e927e42af1
ElHype <78749036+HypeSellit@users.noreply.github.com>
2022-08-04 00:42:01 +0200

    Add files via upload

---
+++ comandos/comandodeprueba.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: "prueba",
+  alias: [],
+  descripcion: "",
+
+execute(client, message, args){
+message.channel.send("Comando de Prueba")
+
+}
+}
